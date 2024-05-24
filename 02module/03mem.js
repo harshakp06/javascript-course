bf4205d57a13630e817f4d060947dbd14f028a3f
Harsha Vardhan <kpharsha@yahoo.com>
2024-05-24 14:33:21 +0530

    add

---
+++ 02module/03mem.js
@@ -0,0 +1,29 @@
+// const name = "harsha"
+
+// const repoc = 50
+
+// console.log(`Hi name is ${name} and my repo count ${repoc}`);
+
+
+// console.log(name[0]);
+
+
+// console.log(name.length);
+
+// let some = "    jkdsnk  jjkesn    "
+
+// console.log(some);
+// console.log(some.trim());
+
+// console.log(some.length);
+// console.log((some.trim()).length);
+
+// console.log(("jkdsnkjjkesn").length);
+
+const sym1 = Symbol();
+const sym2 = Symbol("foo");
+const sym3 = Symbol("foo");
+
+console.log(sym1);
+console.log(sym2);
+console.log(sym3);
---
+++ 02module/04obj.js
@@ -0,0 +1,10 @@
+const arry = [0,8,9,"hjds","(jsdbd)",["j,sjashdj",8526,"%%%%"]]
+
+console.log(arry);
+// console.log(arry[5][1]);
+
+// console.log(arry.indexOf(666));
+
+const newArry = arry.join(" ")
+
+console.log(newArry);
